6bd7ebc0c282f4c47a159b37cdd8a847329eb634
Estudianteucne <Estudianteucne@DESKTOP-VNH07PU>
2023-06-05 20:14:00 -0400

    Add project files.

---
+++ Parcial_1_LisbethTejada/DAL/IngresosContext.cs
@@ -0,0 +1,14 @@
+﻿using Parcial_1_LisbethTejada.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Parcial_1_LisbethTejada.DAL
+{
+    public class IngresosContext : DbContext
+    {
+        public IngresosContext(DbContextOptions<IngresosContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Ingresos> ingresos { get; set; }
+    }
+}
---
+++ Parcial_1_LisbethTejada/Models/Ingresos.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Parcial_1_LisbethTejada.Models
+{
+    public class Ingresos
+    {
+       
+            [Key]
+            public int IngresoId { get; set; }
+
+            [Required(ErrorMessage = "La fecha requerida es obligatoria. Ejemplo; 02/06/2023")]
+            public string Fecha { get; set; }
+
+            [Required(ErrorMessage = "El concepto requerido es obligatorio")]
+            public string Concepto { get; set; }
+
+            [Required(ErrorMessage = "El monto requerido es obligatorio. Ejemplo; 5000")]
+            public string Monto { get; set; }
+
+    }
+}
+
+
+
